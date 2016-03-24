4c4c72148d0b23bce78bb77f28b0a9e93a8444ff
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-03-24 21:50:10 +0530

    Add the app

---
+++ components/stories/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../header';
+import {storiesOf, action} from 'react-storybook';
+
+storiesOf('Header', module)
+  .add('default view', () => {
+    return (
+      <div className="todoapp">
+        <Header addTodo={action('Add Todo')}/>
+      </div>
+    );
+  })
+  .add('nothing', () => (<p>Hello Man</p>))
